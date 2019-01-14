5683a1723f42ba1c40696a2f9550c575628b8c6f
Rayleigh <gmj1197@gmail.com>
2019-01-14 19:02:34 +0900

    feat: Add base for react-DeepSpeech

---
+++ index.js
@@ -0,0 +1,10 @@
+const React = require("react");
+
+module.exports = React.createClass({
+  displayName: "DeepSpeech",
+  getDefaultProps: function() {
+    return {};
+  },
+  propTypes: {},
+  componentDidMount: function() {}
+});
