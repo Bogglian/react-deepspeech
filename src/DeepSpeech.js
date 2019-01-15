1d9e0baad44e2b5be2b5af0ea81dad1ae55f5444
Rayleigh <gmj1197@gmail.com>
2019-01-15 22:57:54 +0900

    feat: Add default styling for example

--- src/DeepSpeech.js
+++ src/DeepSpeech.js
@@ -1,4 +1,5 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
+import "./deepspeech.css";
 
 class DeepSpeech extends Component {
   render() {
@@ -10,43 +11,41 @@ class DeepSpeech extends Component {
       onChangeInput
     } = this.props;
     return (
-      <Fragment>
-        <form>
-          <div className="deepSpeech-title">
-            <input
-              type="text"
-              name="title"
-              placeholder="Title"
-              value={title}
-              onChange={onChangeInput}
-            />
-          </div>
-          <div className="deepSpeech-content">
-            <textarea
-              type="text"
-              name="content"
-              placeholder="Leave a comment"
-              value={content}
-              onChange={onChangeInput}
-            />
-          </div>
-          <div className="deepSpeech-upload">
-            <input
-              className="file-name"
-              value={file ? file.name : "select file"}
-              disabled="disabled"
-            />
-            <label for="fileUpload">UPLOAD</label>
-            <input
-              type="file"
-              name="fileUpload"
-              id="fileUpload"
-              onChange={onChangeFile}
-              accept=".wav, .raw, .mp3, .mp4, .flex, .m4a"
-            />
-          </div>
-        </form>
-      </Fragment>
+      <form>
+        <div className="deepSpeech-title">
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={title}
+            onChange={onChangeInput}
+          />
+        </div>
+        <div className="deepSpeech-content">
+          <textarea
+            type="text"
+            name="content"
+            placeholder="Leave a comment"
+            value={content}
+            onChange={onChangeInput}
+          />
+        </div>
+        <div className="deepSpeech-upload">
+          <input
+            className="file-name"
+            value={file ? file.name : "select file"}
+            disabled="disabled"
+          />
+          <label for="fileUpload">UPLOAD</label>
+          <input
+            type="file"
+            name="fileUpload"
+            id="fileUpload"
+            onChange={onChangeFile}
+            accept=".wav, .raw, .mp3, .mp4, .flex, .m4a"
+          />
+        </div>
+      </form>
     );
   }
 }
