51b2e6d04f79730337fc02a8dcf0789b60b691d9
Rayleigh <gmj1197@gmail.com>
2019-01-31 03:42:53 +0900

    dev: Add example project

---
+++ example/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { DeepSpeech } from 'react-deepspeech';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <DeepSpeech />
+      </div>
+    );
+  }
+}
+
+export default App;
