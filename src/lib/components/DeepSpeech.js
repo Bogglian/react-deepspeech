e288c30d5b41b5a7a635ebb5ac004f9fb072cdcb
jihun <hojihun5516@daum.net>
2019-01-30 22:39:43 +0900

    fix: Modify for modularization
    We should proceed with the modularization of the deepspeech component
    
    Deepspeech makes the values passed in props
    
    Separate the deepspeech component and the inputFileForm component from the demo version of the app.

--- src/lib/components/DeepSpeech.js
+++ src/lib/components/DeepSpeech.js
@@ -1,50 +1,28 @@
 import React, { Component } from 'react';
 import { SViewer } from './Viewer';
 import { SEditor } from './Editor';
-import { SInputFileForm } from './InputFileForm';
-import * as api from '../api';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/theme/monokai.css';
 
 class DeepSpeech extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      input:
-        '# Headline\n\n## sub-title\n\n* option\n  * sub-option1\n  * sub-option2\n\n> tip\n>\n> tipe contents\n\n\n```\ntest conetnts\n\ntest input\n\n```',
-      file:''
+      input: this.props.value
     };
   }
+
+  componentWillReceiveProps(nextProps){
+    if(this.props.value!==nextProps.value){
+      this.handleBeforeChange(null,null,nextProps.value)
+    }
+  }
+
 
   handleBeforeChange = (editor, data, value) => {
     this.setState({ input: value });
   };
-
-  handleChangeFile = e => {
-    this.setState({
-      file: e.target.files[0],
-    });
-   console.log('file: ' + this.state.file);
-  };
-  
-  handleAddResult = value => {
-    this.setState(prevState => ({input: prevState.input + value}));
-  }
-
-   handleFileSubmit = () => {
-    const formData = new FormData();
-    formData.append("audiofile", this.state.file);
-    const headers = {
-	    headers: {
-              Accept: "application/json",
-              "Content-Type": "multipart/form-data"
-	    }
-    };
-     api.dsFile(formData, headers).then(result => {
-      this.handleAddResult("\n```\n"+result.data.ds+"\n```\n")
-    });
-  }
   
   render() {
     return (
@@ -53,12 +31,7 @@ class DeepSpeech extends Component {
           input={this.state.input}
           onBeforeChange={this.handleBeforeChange}
         />
-        <SInputFileForm handleChangeFile={this.handleChangeFile} />
-
         <SViewer input={this.state.input} />
-        <div>
-          <button onClick={this.handleFileSubmit}>Save</button>
-        </div>
       </div>
     );
   }
